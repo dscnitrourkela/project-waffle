1f373edb32cfbe31a20e0283d9e9ea93dcfa12aa
HIMANSHU6001 <hk9797592893@gmail.com>
2025-01-12 20:57:29 +0530

    chore: fixed navbar

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { H1, Paragraph } from '../shared/typography/Headings';
-import {
-  QuestionsHeadingContainer,
-  ParagrapgContainer,
-  QuestionsContainer,
-  Question,
-  QuestionAnswer,
-  QuestionHeading,
-} from './styles';
-import questions from '@/config/content/questions/index';
+import { QuestionsHeadingContainer, ParagrapgContainer } from './styles';
+import MobileQuestions from './MobileQuestions';
+import DesktopQuestions from './DesktopQuestions';
 
 const Questions = () => {
   const [activeQuestion, setActiveQuestion] = useState(null);
@@ -28,46 +22,8 @@ const Questions = () => {
           need.
         </Paragraph>
       </ParagrapgContainer>
-      <QuestionsContainer className='!hidden md:!grid'>
-        <div className='space-y-6'>
-          {questions.map((question) => {
-            if (question.id % 2 !== 0) {
-              return (
-                <Question key={question.id}>
-                  <QuestionHeading>{question.question}</QuestionHeading>
-                  <QuestionAnswer>{question.answer}</QuestionAnswer>
-                </Question>
-              );
-            }
-          })}
-        </div>
-        <div className='space-y-6'>
-          {questions.map((question) => {
-            if (question.id % 2 === 0) {
-              return (
-                <Question key={question.id}>
-                  <QuestionHeading>{question.question}</QuestionHeading>
-                  <QuestionAnswer>{question.answer}</QuestionAnswer>
-                </Question>
-              );
-            }
-          })}
-        </div>
-      </QuestionsContainer>
-      <QuestionsContainer className='!grid md:!hidden'>
-        {questions.map((question, index) => (
-          <Question key={index}>
-            <QuestionHeading onClick={() => toggleQuestion(index)}>
-              {question.question}
-            </QuestionHeading>
-            <QuestionAnswer
-              className={`${activeQuestion === index ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'} overflow-hidden transition-all duration-200 ease-in-out`}
-            >
-              {question.answer}
-            </QuestionAnswer>
-          </Question>
-        ))}
-      </QuestionsContainer>
+      <MobileQuestions toggleQuestion={toggleQuestion} activeQuestion={activeQuestion} />
+      <DesktopQuestions />
     </>
   );
 };
--- src/config/content/questions/index.js
+++ src/config/content/questions/index.js
@@ -1,4 +1,4 @@
-const data = [
+export const QUESTIONS_DATA = [
   {
     id: 1,
     question: 'What are the dates for HackNITR 6.0?',
@@ -60,5 +60,3 @@ const data = [
       'Although we will provide you with a lot of things, you should bring your laptop, chargers, extension cords, and any other hardware you might need. Furthermore it is mandatory to bring a physical copy of your undertaking form signed by your college authority and the hacker ID assigned to you within 24 hrs before the event along with a valid govt. ID. For further information please look at Things to carry for HackNITR 6.0 notion link. Students faling to submit the MoU within the stipulated time will be disqualified from the event.',
   },
 ];
-
-export default data;
