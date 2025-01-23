8d642513b6120e7d0db4ae7cf679cd59fe9286e3
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2025-01-23 21:52:41 +0530

    feat: simplify brochure page and update button functionality

--- src/app/brochure/page.js
+++ src/app/brochure/page.js
@@ -1,29 +1,14 @@
 'use client';
 import { PrimaryButton } from '@/components/shared/typography/Button';
 import { SectionContainer } from './style';
-import { useEffect, useState } from 'react';
 
 const Brochure = () => {
-  const [showPDF, setShowPDF] = useState(false);
-  const pdfUrl =
-    'https://drive.google.com/file/d/1PWXc0CSYk6x7xRrRNWAnSsL9LszDqdbP/view?usp=drivesdk';
-
-  useEffect(() => {
-    setShowPDF(true);
-  }, []);
-
   return (
     <SectionContainer>
-      {showPDF ? (
-        <object data={pdfUrl} type='application/pdf' width='100%' height='600px'>
-          <div className='p-4 text-center'>
-            <p className='mb-4'>Unable to display PDF file. You can:</p>
-            <PrimaryButton text='Download PDF' onClick={() => window.open(pdfUrl, '_blank')} />
-          </div>
-        </object>
-      ) : (
-        <PrimaryButton text='See Brochure' onClick={() => setShowPDF(true)} />
-      )}
+      <PrimaryButton
+        text='Open Brochure'
+        onClick={() => window.open('/brochure.pdf', '_blank', 'fullscreen=yes')}
+      />
     </SectionContainer>
   );
 };
--- src/app/brochure/style.js
+++ src/app/brochure/style.js
@@ -1,7 +1,6 @@
-import Image from 'next/image';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const SectionContainer = styled.main`
-  ${tw`flex justify-center items-center`}
+  ${tw`flex justify-center items-center pb-36`}
 `;
