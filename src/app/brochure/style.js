0e1936693922028d2657c8ab1dde8abff29d531b
Dhrutinandan Swain <dhrutiswainnandan@gmail.com>
2025-01-25 13:22:59 +0530

    refactor: Add additional button for viewing and downloading brochure

--- src/app/brochure/style.js
+++ src/app/brochure/style.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const SectionContainer = styled.main`
-  ${tw`flex justify-center items-center pb-36`}
+  ${tw`flex flex-col justify-center items-center h-screen space-y-10 pb-20 sm:pb-36`}
 `;
