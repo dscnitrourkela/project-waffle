bb6c009bdf17789d811a7e4843d8ceb063a80e3a
ayush <ayushatschool5@gmail.com>
2025-02-04 13:58:17 +0530

    feat: add infinite scrolling gallery component

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,13 @@
 import AboutUs from '@/components/about-us/about-us';
 import CampusAmbasador from '@/components/campusAmbasador/CampusAmbasador';
 import CommunityPartnerSection from '@/components/communityPartners/CommunityPartners';
-import Gallery from '@/components/gallery/gallery';
+import InfiniteGallery from '@/components/gallery/infinite-gallery';
 import Hero from '@/components/hero/hero';
 import Prizes from '@/components/prizes/prizes';
 import Questions from '@/components/questions/Questions';
 import { PastSponsors, Sponsors } from '@/components/sponsors/sponsors';
 import { StatsMain } from '@/components/stats/stats-main';
 import Timeline from '@/components/timeline/timeline';
-
-// import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -20,7 +18,7 @@ export default function Home() {
       <AboutUs />
       <StatsMain />
       <Prizes />
-      <Gallery />
+      <InfiniteGallery />
       <Timeline />
       <Sponsors />
       <PastSponsors />
---
+++ src/components/gallery/infinite-gallery.jsx
@@ -0,0 +1,102 @@
+'use client';
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import { GALLERYCONTENT } from '@/config/content/gallery';
+import {
+  GalleryContainer,
+  GalleryHeading,
+  GalleryHeadingContainer,
+  GalleryImage,
+  RowContainer,
+} from './styles';
+
+const InfiniteGallery = () => {
+  const firstRow = [...GALLERYCONTENT.slice(0, Math.floor(GALLERYCONTENT.length / 3))];
+  const secondRow = [
+    ...GALLERYCONTENT.slice(
+      Math.floor(GALLERYCONTENT.length / 3),
+      Math.floor((2 * GALLERYCONTENT.length) / 3),
+    ),
+  ];
+  const thirdRow = [...GALLERYCONTENT.slice(Math.floor((2 * GALLERYCONTENT.length) / 3))];
+
+  return (
+    <>
+      <div className='flex justify-center !mt-32 mb-20'>
+        <GalleryHeadingContainer>
+          <GalleryHeading>Gallery</GalleryHeading>
+        </GalleryHeadingContainer>
+      </div>
+      <GalleryContainer>
+        <Marquee
+          direction='left'
+          speed={40}
+          gradient={false}
+          className='h-full'
+          style={{
+            overflowX: 'visible',
+          }}
+        >
+          <RowContainer>
+            {firstRow.map((image, index) => (
+              <GalleryImage
+                key={`top-${image.id}`}
+                src={image.url}
+                alt={`Gallery image ${index + 1}`}
+                className='w-[400px] h-[225px]'
+                width={500}
+                height={500}
+              />
+            ))}
+          </RowContainer>
+        </Marquee>
+
+        <Marquee
+          direction='right'
+          speed={30}
+          gradient={false}
+          style={{
+            overflowX: 'visible',
+          }}
+        >
+          <RowContainer>
+            {secondRow.map((image, index) => (
+              <GalleryImage
+                key={`middle-${image.id}`}
+                src={image.url}
+                alt={`Gallery image ${index + 1}`}
+                className='h-[325px] w-[500px] '
+                width={500}
+                height={500}
+              />
+            ))}
+          </RowContainer>
+        </Marquee>
+
+        <Marquee
+          direction='left'
+          speed={35}
+          gradient={false}
+          style={{
+            overflowX: 'visible',
+          }}
+        >
+          <RowContainer>
+            {thirdRow.map((image, index) => (
+              <GalleryImage
+                key={`bottom-${image.id}`}
+                src={image.url}
+                alt={`Gallery image ${index + 1}`}
+                className='w-[400px] h-[225px] '
+                width={500}
+                height={500}
+              />
+            ))}
+          </RowContainer>
+        </Marquee>
+      </GalleryContainer>
+    </>
+  );
+};
+
+export default InfiniteGallery;
